04b94639e72a4baef45db91bb70a83a8cdc39dc6
devlitthf <devdevil901@gmail.com>
2024-04-20 12:51:45 +0530

    filter add in five.js file

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -10,8 +10,31 @@ const coding = ["My", "Name", "is", "Devraj"]
 
 // coding.forEach( function (items) {
 //     console.log(items);
+//     // Which function is calling inside forEach loop that is callback function as a form of arrow functon and the function is working inside array that's why it's parameter autometically get item or values of coding array
+// });
+
+// const value = coding.forEach( function (items) {
+//     console.log(items);
     // Which function is calling inside forEach loop that is callback function as a form of arrow functon and the function is working inside array that's why it's parameter autometically get item or values of coding array
 // });
+
+// console.log(value)
+
+const myNums = [1,2,3,54,5,6,4,9]
+
+// filter is also like foreach function but the difference is forEach never returns value but filter do
+const newNums = myNums.filter( (num)=> num > 5 )
+console.log(newNums);
+
+const newNums1 = myNums.filter( (nums) => {
+    nums > 5;
+})
+console.log(newNums1);
+
+// it will return empty array because of explicte return when we use arrow function as a callback function and user curlybracket for function body 
+
+
+// This will through unefinned return in function it's mean forEach never return any falue from the function when we call function
 
 // This is a second variation to write forEach loop
 // coding.forEach( (items) => {
@@ -21,8 +44,8 @@ const coding = ["My", "Name", "is", "Devraj"]
 // We can also pass referance of any function as a parameter in forEach function also
 
 // This is the function what we will pass referance of function to forEach loop
-function printMe(item){
-    console.log(item)
-}
+// function printMe(item){
+//     console.log(item)
+// }
 
-coding.forEach(printMe)
+// coding.forEach(printMe)
