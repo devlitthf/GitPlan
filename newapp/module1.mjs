9764cc6e5a09ab18ba0ee44ee9f491422963b163
devlitthf <devdevil901@gmail.com>
2024-04-28 10:18:50 -0700

    test module created for Default/Named export functionality testing

---
+++ newapp/module1.mjs
@@ -0,0 +1,7 @@
+// import names from './module2.mjs'
+import {a,b,c,d} from './module2.mjs'
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ newapp/module2.mjs
@@ -0,0 +1,13 @@
+const a = "Devraj";
+const b = "Deepak";
+const c = "Dev";
+const d = "Devil";
+
+// This is a default export
+// export default a;
+
+// This is an named export
+export {a};
+export {b};
+export {c};
+export {d};
